bb2afc1dfdca8e1bdfffefbc1705f08200cdbcf2
Kyagara <kevinsouzarocha@outlook.com>
2021-03-19 13:29:06 -0300

    boa cloudflare

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,7 +1,7 @@
 const Header = ({ headerTitle }) => {
     return (
-        <div className="w-full h-96 relative flex flex-1 items-center text-white bg-gray-800 overflow-hidden z-10">
-            <span className="ml-8 sm:ml-20 uppercase font-montserrat text-6xl sm:text-9xl">
+        <div className="w-full h-96 relative flex flex-1 items-center text-white uppercase bg-gray-800 overflow-hidden z-10">
+            <span className="ml-8 sm:ml-20 font-montserrat text-6xl sm:text-9xl">
                 {headerTitle}
             </span>
         </div>
